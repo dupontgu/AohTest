6f606b1d634e0924e861a13a2d176e35e6d1abb1
guyIntrepid <guy@intrepid.io>
2018-03-26 09:00:40 -0400

    add actual documentation

--- src/main/kotlin/io/intrepid/library/AohLibrary.kt
+++ src/main/kotlin/io/intrepid/library/AohLibrary.kt
@@ -1,3 +1,8 @@
 package io.intrepid.library
 
+/**
+ * Do a thing with a thing
+ * @param x Thing to have thing done with
+ * @return The result of doing the thing with the thing
+ */
 fun doThing(x: Int) = x + x
